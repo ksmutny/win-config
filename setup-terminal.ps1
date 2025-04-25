8403490722a8c248421822eda29116dfae26f9bc
Karel Smutny <karel.smutny@outlook.com>
2025-04-25 13:48:55 +0200

    Set PSGallery a trusterd repository

--- setup-terminal.ps1
+++ setup-terminal.ps1
@@ -1,2 +1,4 @@
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
 Install-Module -Name Terminal-Icons -Repository PSGallery
 Install-Module -Name CompletionPredictor -Repository PSGallery
